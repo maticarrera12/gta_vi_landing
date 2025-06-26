a2d4ea1827468406a7f973555a8c766b2b8f3483
MatiCarrera <mcarreradev12@gmail.com>
2025-06-25 21:48:49 -0300

    implemented jason section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger } from "gsap/all"
 import Navbar from "./sections/Navbar"
 import Hero from "./sections/Hero"
 import FirstVideo from "./sections/FirstVideo"
+import Jason from "./sections/Jason"
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -12,6 +13,7 @@ const App = () => {
         <Navbar/>
         <Hero/>
         <FirstVideo/>
+        <Jason/>
     </main>
   )
 }
